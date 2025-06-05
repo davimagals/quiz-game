7b19aaf8ec52125b291190d5bd297907f19f4527
Davi Magalhaes <davimagal.sc@gmail.com>
2025-06-05 14:25:20 -0300

    inicial

--- logica.js
+++ logica.js
@@ -16,41 +16,101 @@ listaPerguntas = [
   },
 ];
 
-pergunta = null;
+objPergunta = null;
+
+telaInicial = document.querySelector(".tela-inicial");
+telaPerg = document.querySelector(".tela-pergunta");
+telaAcertou = document.querySelector(".tela-acertou");
+telaErrou = document.querySelector(".tela-errou");
 
 botaoIniciar = document.querySelector(".iniciar");
 botaoIniciar.addEventListener("click", iniciarJogo);
 
-function iniciarJogo() {
-  telaInicial = document.querySelector(".tela-inicial");
-  telaInicial.style.display = "none";
+divPergunta = document.querySelector(".pergunta");
+botao0 = document.querySelector(".opcao0");
+botao0.addEventListener("click", () => {
+  if (objPergunta.correta == 0) {
+    mostrarAcertou();
+  } else {
+    mostrarErrou();
+  }
+});
 
-  telaPerg = document.querySelector(".tela-pergunta");
+botao1 = document.querySelector(".opcao1");
+botao1.addEventListener("click", () => {
+  if (objPergunta.correta == 1) {
+    mostrarAcertou();
+  } else {
+    mostrarErrou();
+  }
+});
+
+botao2 = document.querySelector(".opcao2");
+botao2.addEventListener("click", () => {
+  if (objPergunta.correta == 2) {
+    mostrarAcertou();
+  } else {
+    mostrarErrou();
+  }
+});
+
+botao3 = document.querySelector(".opcao3");
+botao3.addEventListener("click", () => {
+  if (objPergunta.correta == 3) {
+    mostrarAcertou();
+  } else {
+    mostrarErrou();
+  }
+});
+
+telaAcertou.addEventListener("click", () => {
+  telaAcertou.style.display = "none";
+  iniciarJogo();
+});
+
+telaErrou.addEventListener("click", () => {
+  telaErrou.style.display = "none";
+  telaInicial.style.display = "block";
+});
+
+function iniciarJogo() {
+  telaInicial.style.display = "none";
   telaPerg.style.display = "block";
 
   mostrarPergunta();
 }
 
 function mostrarPergunta() {
-  objPergunta = listaPerguntas[0];
+  aleatorio = Math.floor(Math.random() * listaPerguntas.length);
+  objPergunta = listaPerguntas[aleatorio];
 
   textoPergunta = document.createTextNode(objPergunta.pergunta);
-  divPergunta = document.querySelector(".pergunta");
+  divPergunta.innerHTML = "";
   divPergunta.appendChild(textoPergunta);
 
   txtOpcao1 = document.createTextNode(objPergunta.opcoes[0]);
-  botao0 = document.querySelector(".opcao0");
+  botao0.innerHTML = "";
   botao0.appendChild(txtOpcao1);
 
   txtOpcao2 = document.createTextNode(objPergunta.opcoes[1]);
-  botao1 = document.querySelector(".opcao1");
+  botao1.innerHTML = "";
   botao1.appendChild(txtOpcao2);
 
   txtOpcao3 = document.createTextNode(objPergunta.opcoes[2]);
-  botao2 = document.querySelector(".opcao2");
+  botao2.innerHTML = "";
   botao2.appendChild(txtOpcao3);
 
   txtOpcao4 = document.createTextNode(objPergunta.opcoes[3]);
-  botao3 = document.querySelector(".opcao3");
+  botao3.innerHTML = "";
   botao3.appendChild(txtOpcao4);
+}
+
+function mostrarAcertou() {
+  telaPerg.style.display = "none";
+  telaAcertou.style.display = "block";
+}
+
+function mostrarErrou() {
+  telaPerg.style.display = "none";
+  telaErrou.style.display = "block";
 }
